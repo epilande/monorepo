269ba9d0d61269c3b4f8bba7446f2216f5108eba
Emmanuel Pilande <epilande@gmail.com>
2018-06-02 18:48:54 -0700

    :package: Update webpack config to bundle individual apps

--- apps/foo/webpack.config.js
+++ apps/foo/webpack.config.js
@@ -1,23 +1,17 @@
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const baseConfig = require('./webpack.config.base');
+const baseConfig = require('../../webpack.config.base');
 
 const config = function(env) {
   return merge(baseConfig({ env, dir: __dirname }), {
     entry: {
-      foo: './apps/foo/src/index.js',
-      bar: './apps/bar/src/index.js',
+      foo: './src/index.js',
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './apps/foo/src/index.html',
+        template: './src/index.html',
         filename: 'foo/index.html',
         chunks: ['foo'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './apps/bar/src/index.html',
-        filename: 'bar/index.html',
-        chunks: ['bar'],
       }),
     ],
   });
