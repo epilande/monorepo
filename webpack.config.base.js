77f90b9ece5b481fc26d990caf18a91a16eb2596
Emmanuel Pilande <epilande@gmail.com>
2018-06-02 18:27:01 -0700

    :sparkles: Update base to handle dynamic env & dirname

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,13 +1,20 @@
 const path = require('path');
+const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const devConfig = require('./webpack.config.dev');
+const prodConfig = require('./webpack.config.prod');
 
-module.exports = {
-  output: {
-    filename: '[name]/bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+const baseConfig = function({ env, dir = __dirname }) {
+  const envConfig = env.prod ? prodConfig : devConfig;
 
-  plugins: [
-    new CleanWebpackPlugin(['./dist']),
-  ],
+  return merge(envConfig, {
+    output: {
+      filename: '[name]/bundle.[hash].js',
+      path: path.resolve(dir, 'dist'),
+    },
+
+    plugins: [new CleanWebpackPlugin([path.resolve(dir, 'dist')])],
+  });
 };
+
+module.exports = baseConfig;
